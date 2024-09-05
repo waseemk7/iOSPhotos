4901b8a6dad0ffa4103d50dcda505a9997becf78
Waseem Kurne <kurne.waseem@gmail.com>
2024-09-05 20:22:16 +0530

    header options

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,15 @@
 import { Stack } from "expo-router";
 
 const Rootlayout = () => {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        gestureDirection: "vertical",
+        animation: "fade",
+      }}
+    />
+  );
 };
 
 export default Rootlayout;
